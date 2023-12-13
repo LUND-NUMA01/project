37ca0ab5e96ecc8585303882dd325b9b7b282fce
Luca Lewin <luca.lewin23@gmail.com>
2023-12-13 13:28:25 +0100

    replace old task 2

--- src/main.py
+++ src/main.py
@@ -21,45 +21,39 @@ s0 = 9  # m/s
 # the air resistance and acceleration
 C = -0.5*p*cw*np.pi/4*d**2/m
 
-z = 0.5
-
-def func_velocity(t, u):
-    s = np.sqrt(u[0]**2 + u[1]**2)
-    ax = C * s * u[0] * z
-    ay = C * s * u[1] * z**2 - g
-    return np.array([ax,ay])
-
 def solve_with_euler(z0, a0, s0=9, x0=0, y0=1.75):
     T = 1
-    N = 128
+    N = 32
 
     # compute initial x,y velocities
     vx0 = s0 * np.cos(a0)
     vy0 = s0 * np.sin(a0)
     
-    # create arrays for the initial values
-    initial_velocity = np.array([vx0, vy0])
-    initial_positions = np.array([x0, y0])
-
-    # compute all velocities
-    (vt, vu) = explicit_euler(func_velocity, T, N, initial_velocity)
-
-    # do some sketchy stuff
-    h = T / N
-    values = np.empty(N + 1, dtype=object)
-    values[0] = initial_positions
-    # compute positions using the velocities
-    for i in range(1, N + 1):
-        values[i] = (values[i-1] + vu[i-1]*h*z)
+    # create array for the initial values
+    initial_values = np.array([x0, y0, vx0, vy0])
     
-    # extract x and y values
+    def func(_, u):
+        # u[0] = x-position
+        # u[1] = y-position
+        # u[2] = x-velocity
+        # u[3] = y-velocity
+        s = np.sqrt(u[2]**2 + u[3]**2)
+        ax = C * s * u[2] * z0
+        ay = C * s * u[3] * z0**2 - g
+        vx = u[2] * z0
+        vy = u[3] * z0
+        return np.array([vx, vy, ax, ay])
 
+    _, u = explicit_euler(func, T, N, initial_values)
+
+    # extract x and y values from u
     x = np.zeros(N)
     y = np.zeros(N)
+
     for i in range(N):
-        x[i] = values[i][0]
-        y[i] = values[i][1]
-    
+        x[i] = u[i][0]
+        y[i] = u[i][1]
+
     # return x and y values
     return x, y
 
@@ -68,8 +62,8 @@ def solve_with_euler(z0, a0, s0=9, x0=0, y0=1.75):
 # ---------------------------------------------------------- #
 
 def plot_ball_trajectory():
-    z0 = 1
-    a0 = 0.8
+    z0 = 0.5
+    a0 = 1
     x, y = solve_with_euler(z0, a0)
     plt.xlim([0, 2.5])
     plt.scatter(x, y)
@@ -79,15 +73,19 @@ def plot_ball_trajectory():
 #                           Task 5                           #
 # ---------------------------------------------------------- #
 
-def find_optimal_angle():
-    f = () - xB, yB # function
-    j = ()  # jacobian / derivative
-    # initial guesses
-    a0 = 1.4
-    z0 = 0.8
+def function(p, q):
+    x, y = solve_with_euler(p, q)
+    return [x[-1] - xB, y[-1] - yB]
 
-    # a, z = nonlinear_newtons_method(f, j, a0, z0)
+def jacobian(p, q):
     return
+
+def find_optimal_angle():
+    # initial guesses
+    z0 = 0.5
+    a0 = 1
+
+    return nonlinear_newton(function, jacobian, [z0, a0])
 
 # ---------------------------------------------------------- #
 #                           Tests                            #
@@ -95,3 +93,5 @@ def find_optimal_angle():
 
 if __name__ == "__main__":
     plot_ball_trajectory()
+    # z, a = find_optimal_angle()
+    # print(f'time: {1/z}, angle: {a}')
